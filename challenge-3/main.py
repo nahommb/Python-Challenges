f8f2138762f599eddeff278294e54c1e2d8f1643
Nahom Melese <nahomjr17@gmail.com>
2024-02-20 11:30:02 -0800

    some changes

--- challenge-3/main.py
+++ challenge-3/main.py
@@ -2,11 +2,12 @@ print("leap year calculator\n")
 
 year = int(input('enter year\n'))
 if year%4==0:
-    if year%100!=0:
-        print('leap year')
-    elif year%400==0:
-        print('leap')
+    if year%100==0:
+        if year%400==0:
+            print('leap')
+        else:
+            print('not leap')
     else:
-        print('not leap')
+        print('leap')
 else:
     print('not leap')
