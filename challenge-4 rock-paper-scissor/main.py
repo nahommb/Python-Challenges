7791677c169e24dcf0e83d7e7c9ee4c845ce996e
Nahom Melese <nahomjr17@gmail.com>
2024-02-21 14:01:54 -0800

    challenge

---
+++ challenge-4 rock-paper-scissor/main.py
@@ -0,0 +1,26 @@
+import random
+
+user_input = int(input('what do you choose : type 0 for Rock, 1 for paper, 2 for Scissors\n'))
+comp_val = random.randint(0,2)
+print(comp_val)
+if user_input==0:
+    if comp_val == 1:
+        print('lost')
+    elif comp_val == 0:
+        print('equal')
+    else:
+        print('you won')
+elif user_input==1:
+    if comp_val == 2:
+        print('lost')
+    elif comp_val == 1:
+        print('equal')
+    else:
+        print('you won')
+elif user_input==2:
+    if comp_val == 0:
+        print('lost')
+    elif comp_val == 2:
+        print('equal')
+    else:
+        print('you won')
