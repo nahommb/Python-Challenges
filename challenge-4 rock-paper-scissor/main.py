d09e9448f31c9d1367cc4a8cdaac069ff3e3616a
Nahom Melese <nahomjr17@gmail.com>
2024-02-21 14:26:31 -0800

    challenge

--- challenge-4 rock-paper-scissor/main.py
+++ challenge-4 rock-paper-scissor/main.py
@@ -2,28 +2,17 @@ import random
 
 user_input = int(input('what do you choose : type 0 for Rock, 1 for paper, 2 for Scissors\n'))
 comp_val = random.randint(0,2)
-print(f'computer chooses{comp_val}')
+print(f'computer chooses {comp_val}')
 
 if user_input >=3 or user_input<0:
     print('invalid input')
-elif user_input==0:
-    if comp_val == 1:
-        print('lost')
-    elif comp_val == 0:
-        print('equal')
-    else:
-        print('you won')
-elif user_input==1:
-    if comp_val == 2:
-        print('lost')
-    elif comp_val == 1:
-        print('equal')
-    else:
-        print('you won')
-elif user_input==2:
-    if comp_val == 0:
-        print('lost')
-    elif comp_val == 2:
-        print('equal')
-    else:
-        print('you won')
+elif user_input==0 and comp_val == 2:
+    print('you won')
+
+elif user_input==2 and comp_val == 0:
+    print('you lost')
+
+elif comp_val > user_input:
+    print('you lost')
+elif comp_val == user_input:
+    print('draw')
