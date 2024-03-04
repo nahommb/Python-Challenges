9a0f8117da02bdfbe272dbbef571e7e09f8ff8e4
Nahom Melese <nahomjr17@gmail.com>
2024-03-04 09:22:41 -0800

    some changes

--- challenge-9 simple calculator/main.py
+++ challenge-9 simple calculator/main.py
@@ -19,34 +19,24 @@ oprerations = {
     '/':div
 }
 
-num1 = float(input('Enter First Number\n'))
-for symbol in oprerations:
-    print(symbol)
-user_choice = input('choice operation')
-num2 = float(input('Enter Second Number \n'))
+def calculator():
+    num1 = float(input('Enter First Number\n'))
+    for symbol in oprerations:
+        print(symbol)
 
-calc = oprerations[user_choice]
-calc(num1,num2)
+    next = True
 
+    while next:
+        user_choice = input('choice operation')
+        num2 = float(input('Enter Second Number \n'))
 
-
-next = True
-
-# while next:
-#     num1 = float(input('Enter First Number\n'))
-#     user_choice = input('choice operation')
-#     num2 = float(input('Enter Second Number \n'))
-#     result =0
-#     if user_choice =='+':
-#        result = add(num1, num2)
-#     elif user_choice =='-':
-#         result = sub(num1,num2)
-#     elif user_choice =='*':
-#        result = mul(num1,num2)
-#     elif user_choice =='/':
-#        result = div(num1,num2)
-#     else:
-#         print('invalid operator')
-#     can_cantin = input(f"Type 'y to continue calculating with {result} or 'n' to start new calaculation\n")
-#     if can_cantin == 'n':
-#         next = False
+        calc = oprerations[user_choice]
+        answer = calc(num1,num2)
+        # print('invalid operator')
+        can_cantin = input(f"Type 'y to continue calculating with {answer} or 'n' to start new calaculation\n")
+        if can_cantin == 'n':
+            next = False
+            calculator()
+        else:
+            num1 = answer
+calculator()
