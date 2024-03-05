719008e4fcfe432a625e33bf09f4e1b06624046a
Nahom Melese <nahomjr17@gmail.com>
2024-03-05 15:09:15 -0800

    some changes

--- challenge-10 black-jack game/main.py
+++ challenge-10 black-jack game/main.py
@@ -6,13 +6,6 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
-user_card = []
-computer_card =[]
-
-
-for i in range(2):
-    user_card.append(deal_card())
-    computer_card.append(deal_card())
 
 
 def calc_score(cards):
@@ -22,3 +15,19 @@ def calc_score(cards):
         cards.remove(11)
         cards.append(1)
     return sum(cards)
+
+user_card = []
+computer_card =[]
+
+
+for i in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
+
+user_score = calc_score(user_card)
+computer_score = calc_score(computer_card)
+print(f'your score = {user_score}')
+print(f'computer score {computer_score}')
+
+if user_score == 0 or computer_score == 0 or user_card>21:
+    isgame_over = True
