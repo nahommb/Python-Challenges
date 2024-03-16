5dc59ac84a67dc4b92db7e459370f23c12a6adbd
Nahom Melese <nahomjr17@gmail.com>
2024-03-15 22:14:53 -0700

    some changes

--- challenge-12 higher lower game/main.py
+++ challenge-12 higher lower game/main.py
@@ -30,7 +30,7 @@ list_of_celeb = [
 
 def gameGenerator():
     person1 = random.choice(list_of_celeb)
-    list_of_celeb.remove(person1)
+    # list_of_celeb.remove(person1)
     person2 = random.choice(list_of_celeb)
     return {'A':person1,"B":person2}
 
@@ -46,21 +46,23 @@ def game():
 
     while right:
         second_person = gameGenerator()['B']
-        # nextPerson =
+        if(second_person['name']==first_person['name']):
+            second_person = gameGenerator()['B']
+            print(second_person)
         print(f"A : {first_person['name']}\n VS")
         print(f"B : {second_person['name']}")
         usre_chooese = input('who has more followers\n')
-        if first_person['followers'] >= second_person['followers'] and usre_chooese=='a':
+        if first_person['followers'] > second_person['followers'] and usre_chooese=='a':
             score+=1
-            print(f'you get your score = {score}')
+            print(f'you get, your score = {score}')
 
-        elif second_person['followers'] >= first_person['followers'] and usre_chooese=='b':
+        elif second_person['followers'] > first_person['followers'] and usre_chooese=='b':
             score += 1
-            print(f'you get your score = {score}')
+            print(f'you get, your score = {score}')
             first_person = second_person
         else:
             print(f'you lost your score = {score}')
             right =False
-        list_of_celeb.append(first_person)
+        # list_of_celeb.append(first_person)
 
 game()
