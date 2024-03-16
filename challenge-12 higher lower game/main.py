ed3e8e012af0eff7046f85331e046239bf2c53e0
Nahom Melese <nahomjr17@gmail.com>
2024-03-15 19:54:30 -0700

    new challenge

---
+++ challenge-12 higher lower game/main.py
@@ -0,0 +1,32 @@
+import random
+list_of_celeb =[
+    {
+        'name':'Neymar',
+        'nationality':'Brazil',
+        'profession':'footballer',
+        'followers': 38000000
+    },
+    {
+        'name': 'Ronaldo',
+        'nationality': 'Portugal',
+        'profession': 'footballer',
+        'followers': 58000000
+    },
+    {
+        'name': 'Messi',
+        'nationality': 'Argentina',
+        'profession': 'footballer',
+        'followers': 40000000
+    },
+    {
+        'name': 'Salah',
+        'nationality': 'Egypt',
+        'profession': 'footballer',
+        'followers': 18000000
+    },
+]
+pers = random.choice(list_of_celeb)
+print(pers['name']);
+
+# def comper():
+#
