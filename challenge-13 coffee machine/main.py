bde12292407ab3a8061e9a6d58e4aeeb78003e7e
Nahom Melese <nahomjr17@gmail.com>
2024-03-23 15:38:43 -0700

    some changes

--- challenge-13 coffee machine/main.py
+++ challenge-13 coffee machine/main.py
@@ -2,73 +2,68 @@ import machine_data
 
 menu = machine_data.menu
 resource = machine_data.resources
-def reporter():
-   for resources_list in resource:
-       print(f'{resources_list}:{resource[resources_list]}')
 
-user_choose = input('What would you like ?(espresso/latte/cappuccino)')
+def coffee_machine():
 
-def calculator(quarters,dimes,nickles,pennies):
-    sum = pennies*0.01 + dimes*0.1 + nickles*0.05 + quarters*0.25
-    return sum
+    user_choose = input('What would you like ?(espresso/latte/cappuccino)')
+    def reporter():
+       for resources_list in resource:
+           print(f'{resources_list}:{resource[resources_list]}')
+    def calculator(quarters,dimes,nickles,pennies):
+        sum = pennies*0.01 + dimes*0.1 + nickles*0.05 + quarters*0.25
+        return sum
+    def resource_checker(userchoose):
+        isAvialable_resource = False
 
+        if userchoose != 'espresso':
 
+            for available_resource in resource:
+                if resource[available_resource] >= menu[userchoose]['ingredients'][available_resource]:
+                    # print('yess')
+                    isAvialable_resource = True
+                else:
+                    # print('nooo')
+                    isAvialable_resource = False
 
-def resource_checker(userchoose):
-
-    isAvialable_resource = False
-
-    if userchoose != 'espresso':
-
-        for available_resource in resource:
-            if resource[available_resource] >= menu[userchoose]['ingredients'][available_resource]:
-                print('yess')
+        else:
+            if resource['water']>=menu[userchoose]['ingredients']['water'] and resource['coffee']>=menu[userchoose]['ingredients']['coffee']:
+                # print('espriesssso yess')
                 isAvialable_resource = True
             else:
-                print('nooo')
-                isAvialable_resource = False
+                print('noo esprso')
 
-    else:
-        if resource['water']>=menu[userchoose]['ingredients']['water'] and resource['coffee']>=menu[userchoose]['ingredients']['coffee']:
-            print('espriesssso yess')
-            isAvialable_resource = True
-        else:
-            print('noo esprso')
+        return isAvialable_resource
 
-    return isAvialable_resource
+    def coffe_maker():
+            if(user_choose!='report' and resource_checker(user_choose)):
+                print('please insert coins')
+                quarters = float(input('How many quarters\n'))
+                dimes = float(input('How many dimes\n'))
+                nickles = float(input('How many nickles\n'))
+                pennies = float(input('How many pennies\n'))
 
-
-
-# resource_checker(user_choose)
-def coffe_maker():
-
-
-        if(resource_checker(user_choose)):
-            print('please insert coins\n')
-            quarters = float(input('How many quarters\n'))
-            dimes = float(input('How many dimes\n'))
-            nickles = float(input('How many nickles\n'))
-            pennies = float(input('How many pennies\n'))
-
-            converted_amount = calculator(quarters=quarters, dimes=dimes, nickles=nickles, pennies=pennies)
-            print(f'Here is ${converted_amount} in charge')
-            if converted_amount >= menu[user_choose]['cost']:
-                print(user_choose)
-                water_left = resource['water'] - menu[user_choose]['ingredients']['water']
-                coffee_left = resource['coffee'] - menu[user_choose]['ingredients']['coffee']
-                if(user_choose!='espresso'):
-                    milk_left = resource['milk'] - menu[user_choose]['ingredients']['milk']
-                    resource['milk'] = milk_left
-                resource['water'] = water_left
-                resource['coffee'] = coffee_left
+                converted_amount = calculator(quarters=quarters, dimes=dimes, nickles=nickles, pennies=pennies)
+                print(f'Here is ${converted_amount} in charge')
+                if converted_amount >= menu[user_choose]['cost']:
+                    print(f'Here is your {user_choose} enjoy')
+                    water_left = resource['water'] - menu[user_choose]['ingredients']['water']
+                    coffee_left = resource['coffee'] - menu[user_choose]['ingredients']['coffee']
+                    if(user_choose!='espresso'):
+                        milk_left = resource['milk'] - menu[user_choose]['ingredients']['milk']
+                        resource['milk'] = milk_left
+                    resource['water'] = water_left
+                    resource['coffee'] = coffee_left
+                else:
+                    print('you have not enough money')
+            elif user_choose =='report':
+                reporter()
             else:
-                print('you have not enough money')
-        else:
-            print("not Enough resources")
-        # print(resource['water'])
+                print("not Enough resources")
+            # print(resource['water'])
 
-if user_choose == 'report':
-    reporter()
+    coffe_maker()
 
-coffe_maker()
-reporter()
+user_need = 'y'
+while user_need=='y':
+    coffee_machine()
+    user_need = input('Do you need another ? "Y" or "N"')
