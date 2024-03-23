3d78fed8d2761cdbfedef912d16b9e6d0e7d6f89
Nahom Melese <nahomjr17@gmail.com>
2024-03-23 15:14:42 -0700

    some changes

--- challenge-13 coffee machine/machine_data.py
+++ challenge-13 coffee machine/machine_data.py
@@ -27,5 +27,5 @@ menu = {
 resources ={
     'water':300,
     'milk':200,
-    'coffee':100
+    'coffee':50
 }
--- challenge-13 coffee machine/main.py
+++ challenge-13 coffee machine/main.py
@@ -12,37 +12,59 @@ def calculator(quarters,dimes,nickles,pennies):
     sum = pennies*0.01 + dimes*0.1 + nickles*0.05 + quarters*0.25
     return sum
 
+
+
 def resource_checker(userchoose):
-    for i in range(len(resource)):
-        if userchoose!='espresso':
-            if resource[i] >= menu[userchoose]['ingredient'][i]:
+
+    isAvialable_resource = False
+
+    if userchoose != 'espresso':
+
+        for available_resource in resource:
+            if resource[available_resource] >= menu[userchoose]['ingredients'][available_resource]:
                 print('yess')
-                return True
+                isAvialable_resource = True
+            else:
+                print('nooo')
+                isAvialable_resource = False
+
+    else:
+        if resource['water']>=menu[userchoose]['ingredients']['water'] and resource['coffee']>=menu[userchoose]['ingredients']['coffee']:
+            print('espriesssso yess')
+            isAvialable_resource = True
         else:
-            if i==2:
-                return True
-            
+            print('noo esprso')
 
-resource_checker()
+    return isAvialable_resource
+
+
+
+# resource_checker(user_choose)
 def coffe_maker():
-    print('please insert coins\n')
-    quarters = float(input('How many quarters\n'))
-    dimes = float(input('How many dimes\n'))
-    nickles = float(input('How many nickles\n'))
-    pennies = float(input('How many pennies\n'))
 
-    converted_amount = calculator(quarters=quarters,dimes=dimes,nickles=nickles,pennies=pennies)
-    print(f'${converted_amount}')
-    if converted_amount >= menu[user_choose]['cost']:
-        resource_checker()
-        print(user_choose)
-        water_left = resource['water'] - menu[user_choose]['ingredients']['water']
-        coffee_left = resource['coffee'] - menu[user_choose]['ingredients']['coffee']
-        if(user_choose!='espresso'):
-            milk_left = resource['milk'] - menu[user_choose]['ingredients']['milk']
-            resource['milk'] = milk_left
-        resource['water'] = water_left
-        resource['coffee'] = coffee_left
+
+        if(resource_checker(user_choose)):
+            print('please insert coins\n')
+            quarters = float(input('How many quarters\n'))
+            dimes = float(input('How many dimes\n'))
+            nickles = float(input('How many nickles\n'))
+            pennies = float(input('How many pennies\n'))
+
+            converted_amount = calculator(quarters=quarters, dimes=dimes, nickles=nickles, pennies=pennies)
+            print(f'Here is ${converted_amount} in charge')
+            if converted_amount >= menu[user_choose]['cost']:
+                print(user_choose)
+                water_left = resource['water'] - menu[user_choose]['ingredients']['water']
+                coffee_left = resource['coffee'] - menu[user_choose]['ingredients']['coffee']
+                if(user_choose!='espresso'):
+                    milk_left = resource['milk'] - menu[user_choose]['ingredients']['milk']
+                    resource['milk'] = milk_left
+                resource['water'] = water_left
+                resource['coffee'] = coffee_left
+            else:
+                print('you have not enough money')
+        else:
+            print("not Enough resources")
         # print(resource['water'])
 
 if user_choose == 'report':
