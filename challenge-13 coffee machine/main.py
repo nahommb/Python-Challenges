daab2204e57178f6011aac96d36da82011c9f010
Nahom Melese <nahomjr17@gmail.com>
2024-03-23 10:48:43 -0700

    some changes

--- challenge-13 coffee machine/main.py
+++ challenge-13 coffee machine/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import machine_data
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+menu = machine_data.menu
+resource = machine_data.resources
+def reporter():
+   for resources_list in resource:
+       print(resources_list,resource[resources_list])
 
+user_choose = input('What would you like ?(espresso/latte/cappuccino)')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if user_choose == 'report':
+    reporter()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
