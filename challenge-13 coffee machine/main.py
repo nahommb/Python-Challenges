f83708fb39e9e1d566e2b71e9797bcdd7484026b
Nahom Melese <nahomjr17@gmail.com>
2024-03-23 11:03:36 -0700

    some changes

--- challenge-13 coffee machine/main.py
+++ challenge-13 coffee machine/main.py
@@ -4,10 +4,23 @@ menu = machine_data.menu
 resource = machine_data.resources
 def reporter():
    for resources_list in resource:
-       print(resources_list,resource[resources_list])
+       print(f'{resources_list}:{resource[resources_list]}')
 
 user_choose = input('What would you like ?(espresso/latte/cappuccino)')
 
+def calculator(quarters,dimes,nickles,pennies):
+    sum = pennies*0.01 + dimes*0.1 + nickles*0.05 + quarters*0.25
+    return sum
+def coffe_maker():
+    print('please insert coins\n')
+    quarters = float(input('How many quarters\n'))
+    dimes = float(input('How many dimes\n'))
+    nickles = float(input('How many nickles\n'))
+    pennies = float(input('How many pennies\n'))
+
+    converted_amount = calculator(quarters=quarters,dimes=dimes,nickles=nickles,pennies=pennies)
+    print(f'${converted_amount}')
 if user_choose == 'report':
     reporter()
 
+coffe_maker()
