3493050c90f2039ecad1ca574a47b1c79ee4e4b6
Nahom Melese <nahomjr17@gmail.com>
2024-04-03 12:34:07 -0700

    some updates

--- challenge-15 hirst painting/main.py
+++ challenge-15 hirst painting/main.py
@@ -1,4 +1,12 @@
 import colorgram
+from turtle import Turtle , Screen
+
+tr = Turtle()
+sc = Screen()
+
+tr.speed('fastest')
+tr.penup()
+tr.goto(-40,0)
 
 colors = colorgram.extract('image.jpg',6)
 all_colors = []
@@ -11,3 +19,19 @@ for individual_color in colors:
 
 
 print(all_colors)
+color_list = [[(250, 247, 244), (248, 245, 246), (213, 154, 96), (52, 107, 132), (179, 77, 31), (202, 142, 31)]]
+def drawer():
+    for i in range(10):
+        tr.pendown()
+        tr.circle(10)
+        tr.penup()
+        tr.forward(40)
+        tr.pendown()
+
+for i in range(8):
+    tr.pendown()
+    drawer()
+    tr.penup()
+    tr.setposition(0,50*i)
+
+sc.exitonclick()
