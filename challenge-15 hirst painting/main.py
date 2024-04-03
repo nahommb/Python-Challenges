9cd2632dba52e9fb98eeee319e56bf9dadb8bffc
Nahom Melese <nahomjr17@gmail.com>
2024-04-03 13:18:58 -0700

    some updates

--- challenge-15 hirst painting/main.py
+++ challenge-15 hirst painting/main.py
@@ -1,37 +1,28 @@
-import colorgram
-from turtle import Turtle , Screen
+import random
+import turtle as Turtile_module
 
-tr = Turtle()
-sc = Screen()
+tr = Turtile_module.Turtle()
+sc = Turtile_module.Screen()
+
+Turtile_module.colormode(255)
 
 tr.speed('fastest')
 tr.penup()
-tr.goto(-40,0)
-
-colors = colorgram.extract('image.jpg',6)
-all_colors = []
-
-for individual_color in colors:
-    r = individual_color.rgb.r
-    g = individual_color.rgb.g
-    b = individual_color.rgb.b
-    all_colors.append((r,g,b))
 
 
-print(all_colors)
-color_list = [[(250, 247, 244), (248, 245, 246), (213, 154, 96), (52, 107, 132), (179, 77, 31), (202, 142, 31)]]
+color_list = [(20, 247, 44), (208, 245, 246), (213, 154, 96), (52, 107, 32), (179, 177, 31), (202, 142, 31)]
 def drawer():
     for i in range(10):
         tr.pendown()
-        tr.circle(10)
+        tr.dot(20,random.choice(color_list))
         tr.penup()
         tr.forward(40)
         tr.pendown()
 
 for i in range(8):
-    tr.pendown()
+    tr.setposition(0,50*i)
     drawer()
     tr.penup()
-    tr.setposition(0,50*i)
+
 
 sc.exitonclick()
