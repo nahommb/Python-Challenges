4650571b08a7df81c8e6052d5ecccf8dde8e9f88
Nahom Melese <nahomjr17@gmail.com>
2024-04-10 10:09:50 -0700

    updates

--- challenge-16 race game/main.py
+++ challenge-16 race game/main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import random
 
 sc = Screen()
 
@@ -7,7 +7,9 @@ sc.setup(700,500)
 
 color = ['red','green','blue','black','pink','purple']
 position = [-100,-50,0,50,100,150]
+user_bet = sc.textinput(title='Make your Bet',prompt='Which turtile will win the race ? enter color')
 
+all_turtle = []
 
 for i in range(6):
     tr = Turtle()
@@ -15,5 +17,26 @@ for i in range(6):
     tr.color(color[i])
     tr.penup()
     tr.goto(x=-270,y=position[i])
+    all_turtle.append(tr)
+
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtle:
+        random_move = random.randint(1,10)
+        turtle.forward(random_move)
+        if turtle.xcor()>270:
+            is_race_on=False
+            winning_turtle = turtle.pencolor()
+            if winning_turtle == user_bet:
+                print('you won')
+            else:
+                print(f'you lost, the winner is {turtle.pencolor()}')
 
 sc.exitonclick()
+
+
+
