bc8b7908171907c0a40ef30682d875ddb0abfaa4
Nahom Melese <nahomjr17@gmail.com>
2024-04-18 12:33:21 -0700

    update

--- challenge-17 snake game/main.py
+++ challenge-17 snake game/main.py
@@ -2,10 +2,19 @@ from turtle import Turtle , Screen
 
 tr = Turtle()
 sc = Screen()
+ty = Turtle()
 
 sc.bgcolor('black')
 sc.title("Snake Game")
 
 sc.setup(700,700)
+
+tr.shape('square')
+tr.color('white')
+
+ty.goto(-20,0)
+ty.shape('square')
+ty.color('white')
+
 
 sc.exitonclick()
