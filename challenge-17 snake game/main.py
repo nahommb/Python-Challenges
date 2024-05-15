7806b1ec961698e07993b7e5f1760ee8039e43dc
Nahom Melese <nahomjr17@gmail.com>
2024-05-15 09:36:31 -0700

    updated

--- challenge-17 snake game/main.py
+++ challenge-17 snake game/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle , Screen
 import time
+from snake import Snake
 
 tr = Turtle()
 sc = Screen()
@@ -10,29 +11,19 @@ sc.title("Snake Game")
 
 sc.setup(700,700)
 sc.tracer(0)
-starting_position = [(0,0),(-20,0),(-40,0)]
-
-segment =[]
-
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segment.append(new_segment)
+snake = Snake()
 
 game_is_on = True
+
+sc.listen()
+sc.onkey(snake.up,'Up')
+
+
 
 while game_is_on:
     sc.update()
     time.sleep(0.1)
-    for seg in range(len(segment)-1,0,-1):
-        new_x = segment[seg-1].xcor()
-        new_y = segment[seg-1].ycor()
-        segment[seg].goto(new_x,new_y)
-    segment[0].forward(20)
-    segment[0].left(90)
-
+    snake.move()
 
 
 sc.exitonclick()
---
+++ challenge-17 snake game/snake.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+
+STARTING_POSTION = [(0,0),(-20,0),(-40,0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSTION:
+            new_segment = Turtle('square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.segments[0].forward(20)
+    def up(self):
+        self.segments[0].left(90)
+
